e9a6503897da43a4df48b4accd0664e800e55fdc
Mycicle <mjm.digregorio@gmail.com>
2021-01-01 16:55:55 -0500

    yList: works with arbitrarily deep recursion
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- yamlval/example.py
+++ yamlval/example.py
@@ -5,15 +5,17 @@ from schema.yEnum import yEnum
 from schema.yInt import yInt
 from schema.yString import yString
 from schema.ySchema import ySchema
-
+from schema.yList import yList
 class Names(Enum):
-    mike = "Michel"
+    mike = "Michael"
 
 class Schema(ySchema):
     name = yEnum(Names)
     age = yInt(lower=0, upper=120)
     height = yString(lower=3, upper=8)
-
+    listOlists = yList(yList(yList(yString(), yInt())), yInt(), lower=2)
+    normalList = yList(yString(lower=2), yInt(upper=10))
+    
 with open("example.yml") as f:
     config = Schema.validate_and_load(f)
 
---
+++ yamlval/schema/yList.py
@@ -0,0 +1,60 @@
+from loguru import logger
+from typing import List, Any, Tuple, Optional
+
+from schema.base_classes.MultiType import MultiType
+
+class yList(MultiType):
+    __type__ = list
+    def __init__(self, *children, **bounds):
+        super().__init__(*children, **bounds)
+
+    def inbounds(self, inp: Any) -> bool:
+        inBounds: bool = True
+        if self.lower is not None:
+            if len(inp) < self.lower:
+                inBounds = False
+
+        if self.upper is not None:
+            if len(inp) > self.upper:
+                inBounds = False
+        
+        return inBounds
+
+    def matches(self, inp: Any) -> Tuple[bool, Optional[List[str]]]:
+        match: bool = True
+        err: List[str] = []
+        # check type of inp
+        if not isinstance(inp, self.__type__):
+            match = False
+            err += [f"Input {inp} is type {type(inp)}, expected type {self.__type__}"]
+
+        # check bounds of inp
+        if not self.inbounds(inp):
+            match = False
+            err += [f"Input int <{inp}> is out of bounds:\n \
+                lower: {self.lower if self.lower is not None else 'no lower bound'}\n \
+                upper: {self.upper if self.upper is not None else 'no upper bound'}\n \
+                received: {inp}"]
+        
+        # check internal consistency of children
+        # children are the object defining the restrictions placed upon each item 
+        # of input
+
+        # yList(yList(...), yString(), yInt, yDict(...))
+        for item in inp:
+            acceptableTypes: List[Any] = []
+            foundProperChild: bool = False
+            for child in self.__children__:
+                acceptableTypes.append(child.__type__)
+                if isinstance(item, child.__type__):
+                    foundProperChild = True
+                    matches_child, error = child.matches(item)
+                    if not matches_child:
+                        match = False
+                        err += error
+
+            if not foundProperChild:
+                match = False
+                err += [f"Improper type for item <{item}>. Expected type {acceptableTypes}. Recieved type {type(item)}"]
+        
+        return (match, err if not match else None)
