1f0a341557c1c28e60b26467d3cd45eb57712530
Mycicle <mjm.digregorio@gmail.com>
2021-01-03 00:09:55 -0500

    example: wrapped example in main
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- yamlval/example.py
+++ yamlval/example.py
@@ -1,41 +1,45 @@
-import yaml
+def main():
+    import yaml
 
-from enum import Enum
-from yamlval.yEnum import yEnum
-from yamlval.yInt import yInt
-from yamlval.yFloat import yFloat
-from yamlval.yString import yString
-from yamlval.ySchema import ySchema
-from yamlval.yList import yList
-from yamlval.yDict import yDict
-from yamlval.yAny import yAny
-from yamlval.yNone import yNone
-class Names(Enum):
-    mike = "Michael"
+    from enum import Enum
+    from yamlval.yEnum import yEnum
+    from yamlval.yInt import yInt
+    from yamlval.yFloat import yFloat
+    from yamlval.yString import yString
+    from yamlval.ySchema import ySchema
+    from yamlval.yList import yList
+    from yamlval.yDict import yDict
+    from yamlval.yAny import yAny
+    from yamlval.yNone import yNone
+    class Names(Enum):
+        mike = "Michael"
 
-class Schema(ySchema):
-    name = yEnum(Names)
-    age = yInt(lower=0, upper=120)
-    height = yString(lower=3, upper=8)
-    listOlists = yList(yList(yList(yString(), yInt())), yInt(), lower=2)
-    normalList = yList(yString(lower=2), yInt(upper=10))
-    floatingPoint = yFloat(upper=10)
-    dictOstuff = yDict(
-        (yString(), yInt(), yDict((yString(), yString()))),
-        (yInt(upper=2), yList(yList(yString(upper=7), upper=2))),
-        (yString(), yString()),
-    )
-    anyDict = yDict(
-        (yAny(), yInt(), yString())
-    )
-    noneDict = yDict(
-        (yAny(), yNone())
-    )
-    anyList = yList(yAny())
-    noneList = yList(yNone())
-    noneListPlus = yList(yNone(), yFloat())
-    
-with open("example.yml") as f:
-    config = Schema.validate_and_load(f)
+    class Schema(ySchema):
+        name = yEnum(Names)
+        age = yInt(lower=0, upper=120)
+        height = yString(lower=3, upper=8)
+        listOlists = yList(yList(yList(yString(), yInt())), yInt(), lower=2)
+        normalList = yList(yString(lower=2), yInt(upper=10))
+        floatingPoint = yFloat(upper=10)
+        dictOstuff = yDict(
+            (yString(), yInt(), yDict((yString(), yString()))),
+            (yInt(upper=2), yList(yList(yString(upper=7), upper=2))),
+            (yString(), yString()),
+        )
+        anyDict = yDict(
+            (yAny(), yInt(), yString())
+        )
+        noneDict = yDict(
+            (yAny(), yNone())
+        )
+        anyList = yList(yAny())
+        noneList = yList(yNone())
+        noneListPlus = yList(yNone(), yFloat())
+        
+    with open("example.yml") as f:
+        config = Schema.validate_and_load(f)
 
-print(config)
+    print(config)
+
+if __name__ == "__main__":
+    main()
